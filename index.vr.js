7d460859e11fb99e2279123c9b79e4a99f22ea31
Your Name <sheel.bedi@gmail.com>
2017-05-16 22:20:06 -0400

    modded render

--- index.vr.js
+++ index.vr.js
@@ -25,7 +25,7 @@ export default class WelcomeToVR extends React.Component {
             textAlignVertical: 'center',
             transform: [{translate: [0, 0, -3]}],
           }}>
-          Hello, World.
+          Welcome.
         </Text>
       </View>
     );
